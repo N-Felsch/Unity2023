befb969ebc5417b21783c9fd5c4bb063c4aa8ac0
Nicholas Felsch <nicholasfelsch@outlook.com>
2023-11-02 19:52:11 -0600

    Unity Events Unit 6 Challenge

---
+++ UnityEvents/Assets/Scripts/ImageBehaviour.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageBehaviour : MonoBehaviour
+{
+    private Image imageObj;
+    public GameAction updateAction;
+    public UnityEvent startEvent, updateImageEvent;
+    
+    private void Start()
+    {
+        imageObj = GetComponent<Image>();
+        updateAction.raiseNoArgs += OnUpdate;
+        startEvent.Invoke();
+    }startEvent.Invoke();
+    }
