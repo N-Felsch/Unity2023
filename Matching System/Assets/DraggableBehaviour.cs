ea2470345f1c852335141d74864dd49a16bf1402
Nicholas Felsch <nicholasfelsch@outlook.com>
2023-11-07 21:02:42 -0700

    Continuing to work on Lab.

--- Matching System/Assets/DraggableBehaviour.cs
+++ Matching System/Assets/DraggableBehaviour.cs
@@ -1,13 +1,16 @@
-using System;
+
 using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DraggableBehaviour : MonoBehaviour
 {
 
     private Camera cameraObj;
     public bool draggable;
+    public Vector3 position, offset;
+    public UnityEvent startDragEvent, endDragEvent;
+    
     void Start()
     {
         cameraObj = Camera.main;
@@ -15,17 +18,22 @@ public class DraggableBehaviour : MonoBehaviour
 
     public IEnumerator OnMouseDown()
     {
+        offset = transform.position - cameraObj.ScreenToWorldPoint(Input.mousePosition);
         draggable = true;
-        Debug.Log("Drag");
+        startDragEvent.Invoke();
+        yield return new WaitForFixedUpdate();
 
         while (draggable)
         {
             yield return new WaitForFixedUpdate();
+            position = cameraObj.ScreenToWorldPoint(Input.mousePosition) + offset;
+            transform.position = position;
         }
     }
 
     private void OnMouseUp()
     {
         draggable = false;
+        endDragEvent.Invoke();
     }
 }
